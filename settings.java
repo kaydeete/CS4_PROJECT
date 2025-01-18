0667feb0e4a1a1174629dc5dfc4298c860bb757e
AlanS-Tadano <143724181+AlanoS-Tadano@users.noreply.github.com>
2025-01-18 20:00:46 +0800

    Add files via upload

--- settings.java
+++ settings.java
@@ -5,21 +5,62 @@ import java.awt.event.*;
 public class settings implements MouseListener{
     
     JFrame frame;
-    
+    JLabel start;
+    JLabel settings; 
+    JLabel quit;
+    //ImageIcon gameBG;
+    ImageIcon quiticon;
+    ImageIcon img;
+
     public settings(){
         frame = new JFrame();
+        
+        quiticon=new ImageIcon("Images/quit.png");    //259x80 px
+        img=new ImageIcon(quiticon.getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT));
+        quit=new JLabel(img);
+        
+        JPanel backgroundPanel = new JPanel() {
+            private final Image gameBG = new ImageIcon("Images/mainmenuBG.png").getImage();
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                
+                g.drawImage(gameBG, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        
+        backgroundPanel.setLayout(new BorderLayout());
+        frame.setContentPane(backgroundPanel);
     }
     
     public void setFrame() {
         frame.setLayout(new GraphPaperLayout(new Dimension (10,10)));
-        frame.setSize(700,500);
+        
+        frame.add(quit, new Rectangle (1,1,8,2));
+        
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
         frame.setVisible(true);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
+        
+        addListeners();
+    }
+    
+    public void addListeners() {
+        quit.addMouseListener(this);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        if(e.getSource()==quit){
+            mainmenu ng=new mainmenu();
+            Point p=frame.getLocation();
+            ng.setFrame();
+            ng.frame.setLocation(p);
+            frame.dispose();
+        }
     }
 
     @Override
