48f36175015f176615030e8a28ba2e7da3120762
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2025-01-26 01:25:14 +0800

    Add files via upload

--- maze1.java
+++ maze1.java
@@ -14,10 +14,14 @@ public class maze1 implements MouseListener, KeyListener{
     JLabel chatbox;
     int chatseq=0;
     ImageIcon chatimg;
-    JLabel mapL;
+    
+    int currentTile;
+    JLabel mapL[];
     int map[];
 
-    JLabel amper;
+    ImageIcon amper;
+    ImageIcon img1; //wall
+    ImageIcon img2; //door
 
     ImageIcon gameBG;
     ImageIcon img;
@@ -41,7 +45,18 @@ public class maze1 implements MouseListener, KeyListener{
             1,0,0,1,1,1,1,1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,
             1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,1,1,1,1,
             1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,1,
+            1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,
+            1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,1,
+            1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,1,
+            1,1,1,1,0,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,0,0,1,0,0,0,1,
+            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
+            1,0,0,1,1,1,1,1,1,1,1,0,0,0,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+            1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1
         };
+        currentTile = 1;
         
         imgicon = new ImageIcon("Images/nxtbtn.png");
         img = new ImageIcon(imgicon.getImage().getScaledInstance(198, 90, Image.SCALE_DEFAULT));
@@ -64,10 +79,19 @@ public class maze1 implements MouseListener, KeyListener{
             "Go to the items and click to find the mystery!"
         };
         
-        styleChatField(chat[0]);
-        
-        prevbtn.addMouseListener(this);
-        nxtbtn.addMouseListener(this);
+        mapL=new JLabel[map.length];
+        imgicon=new ImageIcon("Images/brickwall.png");
+        img1=new ImageIcon(imgicon.getImage().getScaledInstance(67, 67, Image.SCALE_DEFAULT));
+        imgicon=new ImageIcon("Images/door.png");
+        img2=new ImageIcon(imgicon.getImage().getScaledInstance(67, 67, Image.SCALE_DEFAULT));
+        imgicon=new ImageIcon("Images/amper.png");
+        amper=new ImageIcon(imgicon.getImage().getScaledInstance(65, 65, Image.SCALE_DEFAULT));
+        for(int i=0;i<map.length;i++){
+            if(map[i]==1) mapL[i]=new JLabel(img1);
+            else if(map[i]==2) mapL[i]=new JLabel(img2);
+            else if(map[i]==3) mapL[i]=new JLabel(amper);
+            else mapL[i]=new JLabel();
+        }
         
         /*JPanel backgroundPanel = new JPanel() {
             private final Image gameBG = new ImageIcon("Images/maze1scrap.png").getImage();
@@ -84,21 +108,40 @@ public class maze1 implements MouseListener, KeyListener{
     }
     
     public void setFrame() {
-        frame.setLayout(new GraphPaperLayout(new Dimension(25, 10)));
+        frame.setLayout(new GraphPaperLayout(new Dimension(35,19)));
         frame.add(prevbtn, new Rectangle(0, 9, 3, 1));
         frame.add(nxtbtn, new Rectangle(22, 9, 3, 1));
         //frame.add(arrow, new Rectangle(4, 7, 3, 2));
         //arrow.setVisible(false);
+        
+        int x=0,y=0,w=2,h=2;
+        for(int i=0;i<map.length;i++){
+            //if(map[i]==1) System.out.println("brick added at "+x+","+y+","+w+","+h);
+            frame.add(mapL[i], new Rectangle(x,y,w,h));
+            x+=2;
+            if((i+1)%12==0){
+                x=0;
+                y+=2;
+            }
+        }
 
         frame.add(chatbox, new Rectangle(1, 9, 26, 10));
         frame.add(chatfield, new Rectangle(0, 9, 26, 2));
         chatfield.setBackground(new Color(51, 44, 42));
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
-        frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
         chatfield.setVisible(true);
+        frame.setVisible(true);
+        frame.addKeyListener(this);
+        
+        addListeners();
+    }
+    
+    public void addListeners() {
+        prevbtn.addMouseListener(this);
+        nxtbtn.addMouseListener(this);
     }
     
     public void styleChatField(String text) {
@@ -110,6 +153,12 @@ public class maze1 implements MouseListener, KeyListener{
         SimpleAttributeSet center = new SimpleAttributeSet();
         StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
         doc.setParagraphAttributes(0, doc.getLength(), center, false);
+    }
+    
+    public boolean checkTile(int n){
+        boolean b=false;
+        if(map[n]==0) b=true;
+        return b;
     }
 
     @Override
@@ -141,22 +190,36 @@ public class maze1 implements MouseListener, KeyListener{
     @Override
     public void mouseExited(MouseEvent e) {}
 
-    private void setElements() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    private void setElements() {}
 
     @Override
-    public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int keyCode = e.getKeyCode();
+        int curr=currentTile;
+        if(keyCode==KeyEvent.VK_UP){
+            if(checkTile(currentTile-12)) currentTile-=12;
+        }
+        else if(keyCode==KeyEvent.VK_DOWN){
+            if(checkTile(currentTile+12)) currentTile+=12;
+        }
+        else if(keyCode==KeyEvent.VK_LEFT){
+            if(checkTile(currentTile-1)) currentTile-=1;
+        }
+        else if(keyCode==KeyEvent.VK_RIGHT){
+            if(checkTile(currentTile+1)) currentTile+=1;
+        }
+        
+        if(curr!=currentTile){
+            map[curr]=0;
+            mapL[curr].setIcon(null);
+            map[currentTile]=4;
+            mapL[currentTile].setIcon(amper);
+        }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public void keyReleased(KeyEvent e) {}
 }
