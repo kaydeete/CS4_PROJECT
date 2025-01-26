2a9b9fb6a1dda10d7962883a04692e08e0e68cb6
AlanS-Tadano <143724181+AlanoS-Tadano@users.noreply.github.com>
2025-01-26 17:00:25 +0800

    Add files via upload

--- maze1.java
+++ maze1.java
@@ -1,225 +1,116 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.text.*;
 
-public class maze1 implements MouseListener, KeyListener{
+public class maze1 implements MouseListener, KeyListener {
     JFrame frame;
-    
-    JLabel nxtbtn;
-    JLabel prevbtn;
-    //JLabel arrow;
-
-    JTextPane chatfield;
-    JLabel chatbox;
-    int chatseq=0;
-    ImageIcon chatimg;
-    
+    JLabel[] mapL;
+    int[] map;
+    ImageIcon img1; // Wall
+    ImageIcon img2; // Door
+    ImageIcon amper; // Player
     int currentTile;
-    JLabel mapL[];
-    int map[];
-
-    ImageIcon amper;
-    ImageIcon img1; //wall
-    ImageIcon img2; //door
-
-    ImageIcon gameBG;
-    ImageIcon img;
-    ImageIcon imgicon;
-    String chat[];
-
-    MyStyle ms;
-    PlaySound click;
-    PlaySound steps;
 
     public maze1() {
         frame = new JFrame();
-        steps = new PlaySound();
-        
+
+        // Initialize map data
         map = new int[]{
-            1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,
-            1,0,0,1,1,1,1,1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,
-            1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,1,1,1,1,
-            1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,1,
-            1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,
-            1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,1,
-            1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,1,
-            1,1,1,1,0,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,0,0,1,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,1,1,1,1,1,1,1,1,0,0,0,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,
-            1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1
+            1,0,0,0,1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,0,0,0,0,1,0,
+            1,0,0,0,0,0,0,0,0,0,1,0,
+            1,0,0,0,0,0,0,1,1,1,1,1,
+            1,0,0,0,0,0,0,0,0,0,1,0,
+            1,0,0,1,1,1,1,1,0,0,1,0,
+            1,0,0,1,0,0,0,0,0,0,1,0,
+            1,1,1,1,1,1,1,1,1,1,1,2
         };
         currentTile = 1;
-        
-        imgicon = new ImageIcon("Images/nxtbtn.png");
-        img = new ImageIcon(imgicon.getImage().getScaledInstance(198, 90, Image.SCALE_DEFAULT));
-        nxtbtn = new JLabel(img);
 
-        imgicon = new ImageIcon("Images/prevbtn.png");
-        img = new ImageIcon(imgicon.getImage().getScaledInstance(198, 90, Image.SCALE_DEFAULT));
-        prevbtn = new JLabel(img);
-        
-        /*imgicon = new ImageIcon("Images/arrow.png");
-        img = new ImageIcon(imgicon.getImage().getScaledInstance(198, 90, Image.SCALE_DEFAULT));
-        arrow = new JLabel(img);*/
-        
-        chatimg = new ImageIcon("Images/chatimg.png");
-        chatbox = new JLabel(chatimg);
-        chatfield = new JTextPane();
-        
-        chat = new String[]{
-            "Inside the breadboard is a MAZE",
-            "Go to the items and click to find the mystery!"
-        };
-        
-        mapL=new JLabel[map.length];
-        imgicon=new ImageIcon("Images/brickwall.png");
-        img1=new ImageIcon(imgicon.getImage().getScaledInstance(67, 67, Image.SCALE_DEFAULT));
-        imgicon=new ImageIcon("Images/door.png");
-        img2=new ImageIcon(imgicon.getImage().getScaledInstance(67, 67, Image.SCALE_DEFAULT));
-        imgicon=new ImageIcon("Images/amper.png");
-        amper=new ImageIcon(imgicon.getImage().getScaledInstance(65, 65, Image.SCALE_DEFAULT));
-        for(int i=0;i<map.length;i++){
-            if(map[i]==1) mapL[i]=new JLabel(img1);
-            else if(map[i]==2) mapL[i]=new JLabel(img2);
-            else if(map[i]==3) mapL[i]=new JLabel(amper);
-            else mapL[i]=new JLabel();
+        // Load images
+        try {
+            img1 = new ImageIcon(new ImageIcon("Images/brickwall.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+            img2 = new ImageIcon(new ImageIcon("Images/door.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+            amper = new ImageIcon(new ImageIcon("Images/amper.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+        } catch (Exception e) {
+            System.out.println("Image loading failed! Using default colors instead.");
         }
-        
-        /*JPanel backgroundPanel = new JPanel() {
-            private final Image gameBG = new ImageIcon("Images/maze1scrap.png").getImage();
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                
-                g.drawImage(gameBG, 0, 0, getWidth(), getHeight(), this);
+
+        // Initialize JLabel array
+        mapL = new JLabel[map.length];
+        for (int i = 0; i < map.length; i++) {
+            mapL[i] = new JLabel();
+            mapL[i].setOpaque(true); // Allow background color to show
+            if (map[i] == 1) {
+                mapL[i].setBackground(Color.BLACK); // Wall
+                if (img1 != null) mapL[i].setIcon(img1);
+            } else if (map[i] == 2) {
+                mapL[i].setBackground(Color.BLUE); // Door
+                if (img2 != null) mapL[i].setIcon(img2);
+            } else if (map[i] == 3) {
+                mapL[i].setBackground(Color.GREEN); // Player
+                if (amper != null) mapL[i].setIcon(amper);
+            } else {
+                mapL[i].setBackground(Color.WHITE); // Empty space
             }
-        };
-        
-        backgroundPanel.setLayout(new BorderLayout());
-        frame.setContentPane(backgroundPanel);*/
+        }
     }
-    
-    public void setFrame() {
-        frame.setLayout(new GraphPaperLayout(new Dimension(35,19)));
-        frame.add(prevbtn, new Rectangle(0, 9, 3, 1));
-        frame.add(nxtbtn, new Rectangle(22, 9, 3, 1));
-        //frame.add(arrow, new Rectangle(4, 7, 3, 2));
-        //arrow.setVisible(false);
-        
-        int x=0,y=0,w=2,h=2;
-        for(int i=0;i<map.length;i++){
-            //if(map[i]==1) System.out.println("brick added at "+x+","+y+","+w+","+h);
-            frame.add(mapL[i], new Rectangle(x,y,w,h));
-            x+=2;
-            if((i+1)%12==0){
-                x=0;
-                y+=2;
-            }
-        }
 
-        frame.add(chatbox, new Rectangle(1, 9, 26, 10));
-        frame.add(chatfield, new Rectangle(0, 9, 26, 2));
-        chatfield.setBackground(new Color(51, 44, 42));
+    public void setFrame() {
+        frame.setLayout(new BorderLayout());
+
+        // Map Panel
+        JPanel mapPanel = new JPanel(new GridLayout(8, 12, 2, 2)); // 8 rows, 12 columns
+        for (JLabel label : mapL) {
+            mapPanel.add(label);
+        }
+        frame.add(mapPanel, BorderLayout.CENTER);
+
+        System.out.println("Frame and map initialized successfully!");
+        
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.addKeyListener(this);
         frame.setUndecorated(true);
+        frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
-        chatfield.setVisible(true);
-        frame.setVisible(true);
-        frame.addKeyListener(this);
-        
-        addListeners();
     }
-    
-    public void addListeners() {
-        prevbtn.addMouseListener(this);
-        nxtbtn.addMouseListener(this);
-    }
-    
-    public void styleChatField(String text) {
-        chatfield.setText(text);
-        chatfield.setFont(new Font("Segoe UI", Font.PLAIN, 30));
-        chatfield.setForeground(Color.WHITE);
-        chatfield.setEditable(false);
-        StyledDocument doc = chatfield.getStyledDocument();
-        SimpleAttributeSet center = new SimpleAttributeSet();
-        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-        doc.setParagraphAttributes(0, doc.getLength(), center, false);
-    }
-    
-    public boolean checkTile(int n){
-        boolean b=false;
-        if(map[n]==0) b=true;
-        return b;
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.getSource() == prevbtn) {
-            if (chatseq - 1 >= 0) {
-                chatseq--;
-                chatfield.setText(chat[chatseq]);
-            }
-
-        } else if (e.getSource() == nxtbtn) {
-            if (chatseq + 1 < chat.length) {
-                chatseq++;
-                chatfield.setText(chat[chatseq]);
-                //arrow.setVisible(true);
-            }        
-        }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {}
-
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-
-    @Override
-    public void mouseEntered(MouseEvent e) {}
-
-    @Override
-    public void mouseExited(MouseEvent e) {}
-
-    private void setElements() {}
-
-    @Override
-    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        int curr=currentTile;
-        if(keyCode==KeyEvent.VK_UP){
-            if(checkTile(currentTile-12)) currentTile-=12;
+        int prevTile = currentTile;
+
+        if (keyCode == KeyEvent.VK_UP && currentTile >= 12 && map[currentTile - 12] == 0) {
+            currentTile -= 12;
+        } else if (keyCode == KeyEvent.VK_DOWN && currentTile < map.length - 12 && map[currentTile + 12] == 0) {
+            currentTile += 12;
+        } else if (keyCode == KeyEvent.VK_LEFT && currentTile % 12 > 0 && map[currentTile - 1] == 0) {
+            currentTile -= 1;
+        } else if (keyCode == KeyEvent.VK_RIGHT && currentTile % 12 < 11 && map[currentTile + 1] == 0) {
+            currentTile += 1;
         }
-        else if(keyCode==KeyEvent.VK_DOWN){
-            if(checkTile(currentTile+12)) currentTile+=12;
-        }
-        else if(keyCode==KeyEvent.VK_LEFT){
-            if(checkTile(currentTile-1)) currentTile-=1;
-        }
-        else if(keyCode==KeyEvent.VK_RIGHT){
-            if(checkTile(currentTile+1)) currentTile+=1;
-        }
-        
-        if(curr!=currentTile){
-            map[curr]=0;
-            mapL[curr].setIcon(null);
-            map[currentTile]=4;
+
+        if (prevTile != currentTile) {
+            map[prevTile] = 0;
+            map[currentTile] = 3;
+            mapL[prevTile].setBackground(Color.WHITE);
+            mapL[currentTile].setBackground(Color.GREEN);
+            mapL[prevTile].setIcon(null);
             mapL[currentTile].setIcon(amper);
         }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {}
+    @Override public void keyTyped(KeyEvent e) {}
+    @Override public void keyReleased(KeyEvent e) {}
+    @Override public void mouseClicked(MouseEvent e) {}
+    @Override public void mousePressed(MouseEvent e) {}
+    @Override public void mouseReleased(MouseEvent e) {}
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
+
+    public static void main(String[] args) {
+        maze1 maze = new maze1();
+        maze.setFrame();
+    }
 }
